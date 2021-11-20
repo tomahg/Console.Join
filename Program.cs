0c90287c4582b5278504c523b6dca7ec918cc80a
Tomas Hensrud Gulla <tomas.h.gulla@novacare.no>
2021-11-20 11:20:00 +0100

    Add the actual joining code

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lineInput;
+            string delimiter = "";
+            while ((lineInput = Console.ReadLine()) != null)
+            {
+                Console.Write(delimiter + lineInput.Trim());
+                delimiter = " ";
+            }
+        }
+    }
+}
